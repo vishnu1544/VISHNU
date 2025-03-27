a917e77ff28f6760177300d83c3eb9706f787250
Vishnu <vishnu.v@navayuvabharatinfotech.com>
2025-03-27 16:34:41 +0530

    datatypes

---
+++ 25_03_25_Javascript/27_03-25_javascript/datypes.js
@@ -0,0 +1,78 @@
+///priumitive-Data Types///
+// Number
+let num = 42;
+const pi = 3.14;
+var age = 25;
+
+console.log(num)
+console.log(pi)
+console.log(25)
+
+// String
+let name = "John";
+const city = 'New York';
+var country = "USA";
+console.log(name)
+console.log(city)
+console.log(country)
+
+// Boolean
+let isActive = true;
+const hasPermission = false;
+var isMember = true;
+console.log(isActive)
+console.log(hasPermission)
+console.log(isMember)
+// Undefined
+let x;
+var y;
+console.log(x)
+console.log(y)
+
+// Null
+let emptyValue = null;
+var notAssigned = null;
+console.log(emptyValue)
+console.log(notAssigned)
+
+// BigInt
+const bigNum = 1234567890123456789n;
+console.log(bigNum)
+
+
+// Symbol
+const uniqueId = Symbol("id");
+////Non-Primitive Data Types
+// Object
+let person = { name: "Alice", age: 25 };
+const car = { brand: "Tesla", model: "Model S" };
+var employee = { id: 101, role: "Developer" };
+console.log(person)
+console.log(car)
+console.log(employee)
+// Array
+let numbers = [1, 2, 3];
+const fruits = ["Apple", "Banana"];
+var colors = ["Red", "Blue", "Green"];
+console.log(numbers)
+console.log(fruits)
+console.log(colors)
+
+// Function
+function greet() {
+  return "Hello";
+}
+const sayHi = () => "Hi!";
+var welcome = function() {
+  return "Welcome!";
+};
+
+// Map
+const map = new Map();
+map.set("key1", "value1");
+
+// Set
+let set = new Set();
+set.add(10);
+set.add(20);
+
